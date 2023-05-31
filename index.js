8b21def66e39472c1985a019ad91ae03ef0433af
Hiransanjeewa <hiransanjeewaa@gmail.com>
2023-05-31 19:02:48 +0530

    Nodeman installed for autorefreshing the server when there are changes

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express =require("express");
 const app=express();
 
 app.get("/",(req,res)=>{
-    res.send("Hello worl");
-    console.log("******")
+    res.send("Hello world");
+   // console.log("******")
 });
 app.listen(3000,()=>console.log("Server is listening ... "));
 
