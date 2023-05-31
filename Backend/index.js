bf9f72532236a1fe21a7d31a53a6ba05f22d2fc1
Hiransanjeewa <hiransanjeewaa@gmail.com>
2023-05-31 19:49:21 +0530

    Structure changed

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,10 @@
 const express =require("express");
 const app=express();
+const Userservice=require("/services/UserservicesImpl")
 
 app.get("/",(req,res)=>{
     res.send("Hello world");
-   // console.log("******")
+    console.log(Userservice.print("Welcome back hiran"))
 });
 app.listen(3000,()=>console.log("Server is listening ... "));
 
