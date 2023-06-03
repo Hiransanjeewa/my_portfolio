e1e2c702ce1750e855d5ad6f6af76bd91ff00e49
Hiransanjeewa <hiransanjeewaa@gmail.com>
2023-06-03 13:45:19 +0530

    homepage comppleted without scripting

--- Frontend/my-portfolio/src/pages/home.js
+++ Frontend/my-portfolio/src/pages/home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Footer from '../components/footer';
 import Header from '../components/header';
 import "./home.css"
-import profile_img from "./my_profile_image.png"
+import profile_img from "./my_profile_image.jpg"
 
 export default function Home() {
   return (
@@ -20,41 +20,38 @@ export default function Home() {
         </head>
  
           
-           
 
-
-
-<div class="container-fluid bg-color2 vh-100" >
+<div class="container-fluid text-light " id='homepage_body' >
   <div class="row ">
-    <div class="col col-lg-8" >
+    <div class="col col-lg-6 " id='contents' >
       
     <span>Hi , I'm Hiran.</span>
-              <h1>
+              <p id='caption-1'>
                 I am a full stack Developer.
-              </h1>
-              <p>
-                I'm a Third year software Engineering Undergraduate of university of kelaniya
+              </p>
+              <p >
+                I'm a Third year software Engineering Undergraduate of University of Kelaniya
               </p>
               <span>
-                <a href='about'>
+                <a href='about' class="text-decoration-none">
                 See more about Me
                 </a>
+                <br/>
                 
               </span>
     </div>
-    <div class="col-md-auto bg-color1">
-    <img src={profile_img} class="img-fluid" alt="profile"/>
+    <div class="col-lg-6 d-flex " id='img_div'>
+    <img src={profile_img} class="img-fluid " alt="profile" />
     </div>
     
   </div>
   
 </div>
-
-          
+<Footer / >
  
 
     </html>
-  <Footer / >
+ 
     </div>
   )
 }
