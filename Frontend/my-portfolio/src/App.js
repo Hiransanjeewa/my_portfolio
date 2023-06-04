1fbdafecfc8f3ea5d9d8a3adb23abb69446d87ff
Hiransanjeewa <hiransanjeewaa@gmail.com>
2023-06-04 19:10:09 +0530

    react modules deleted and reinstalled an error occurred when installing react router

--- Frontend/my-portfolio/src/App.js
+++ Frontend/my-portfolio/src/App.js
@@ -1,17 +1,29 @@
-
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from './pages/home';
 
+
+import Home from './pages/home';
+import AboutMe from './pages/about_me';
+import Experience from './pages/experience';
+import Blogs from './pages/blogs';
+
+ 
 
 
 function App() {
   return (
     <div>
-  
-  <Home/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<AboutMe />}/>
+        <Route path="/blogs" element={<Blogs />}/>
+        <Route path="/experience" element={<Experience />}/>
+       
+      </Routes>
+    </BrowserRouter>
     </div>
-  
   );
 }
-
 export default App;
