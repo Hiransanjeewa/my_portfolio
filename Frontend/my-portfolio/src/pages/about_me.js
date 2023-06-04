9d9e5c7361dc1df732a84c92409799be5db25be6
Hiransanjeewa <hiransanjeewaa@gmail.com>
2023-06-04 21:39:25 +0530

    about me page completed need to add real conetents  and picture

--- Frontend/my-portfolio/src/pages/about_me.js
+++ Frontend/my-portfolio/src/pages/about_me.js
@@ -1,73 +1,69 @@
-import React from 'react'
-import "./about_me.css"
-import Header from '../components/header'
-import Footer from '../components/footer'
-import profile_img from "./my_profile_image.jpg"
+import React from "react";
+import "./about_me.css";
+import Header from "../components/header";
+import Footer from "../components/footer";
+import profile_img from "./my_profile_image.jpg";
 
 export default function AboutMe() {
   return (
-    
-<div id='main_div'>
-<Header/>
+    <div id="main_div">
+      <Header />
 
-<div class="container text-center mw-100" id='second_main_div' >
-  <div class="row justify-content-md-center">
-    <div class="col col-lg-6">
-    <p id='main_caption'>
-         <strong><span id='caption_span1'> ABOUT </span> <span id='caption_span2'>ME</span></strong>
-    </p>
+      <div class="container text-center mw-100" id="second_main_div">
+        <div class="row justify-content-md-center">
+          <div class="col col-lg-6">
+            <p id="main_caption">
+              <strong>
+                <span id="caption_span1"> ABOUT </span>{" "}
+                <span id="caption_span2">ME</span>
+              </strong>
+            </p>
+          </div>
+        </div>
+        <div class="row justify-content-md-center">
+          <div class="col col-lg-6" id="second_caption">
+            I'm Hiran Sanjeewa. Third year Software Engineering Undergraduate
+            from University of Kelaniya. Full Stack developer
+          </div>
+        </div>
+      </div>
+
+<div>
+      <div class="container-fluid text-light" id=" ">
+        <div class="row ">
+          <div class="col col-lg-6">
+            <img
+              src={profile_img}
+              class="img-fluid "
+              alt="profile"
+              id="image"
+            />
+          </div>
+          <div class="col-lg-6 d-flex " id=" ">
+            <p id="contents">
+              <span id="content_caption">A Little About Myself</span>
+              <br />
+              <br />
+              Debbie had taken George for granted for more than fifteen years
+              now. He wasn't sure what exactly had made him choose this time and
+              place to address the issue, but he decided that now was the time.
+              <br />
+              <br /> He looked straight into her eyes and just as she was about
+              to speak, turned away and walked out the door. Sarah watched the
+              whirlpool mesmerized.
+              <br /> <br />
+              She couldn't take her eyes off the water swirling around and
+              around. She stuck in small twigs and leaves to watch the whirlpool
+              catch them and then suck them down. It bothered her more than a
+              little bit that this could also be used as a metaphor for her
+              life.
+            </p>
+          </div>
+          <Footer/>
+        </div>
+      </div>
+      </div>
       
     </div>
-  
-  </div>
-  <div class="row justify-content-md-center">
-  <div class="col col-lg-6" id='second_caption'>
-      I'm Hiran Sanjeewa. Third year Software Engineering Undergraduate from University of Kelaniya. Full Stack developer 
-    </div>
-    </div>
-
-   
-  </div>
-
- 
-
-  <div class="container-fluid text-light" id='homepage_body' >
-  <div class="row ">
-    <div class="col col-lg-6" id=' ' >
-      
-   
-   
-    
-    
-    <img src={profile_img} class="img-fluid " alt="profile" id='image'/>
-
-
-
-    </div>
-    <div class="col-lg-6 d-flex " id=' '>
-  
-
-
-    <p id='contents'>
-    <span id='content_caption'>A Little About Myself</span><br/><br/>
-      Debbie had taken George for granted for more than fifteen years now. He wasn't sure what exactly had made him choose this time and place to address the issue, but he decided that now was the time.<br/><br/> He looked straight into her eyes and just as she was about to speak, turned away and walked out the door.
-Sarah watched the whirlpool mesmerized.<br/> <br/>She couldn't take her eyes off the water swirling around and around. She stuck in small twigs and leaves to watch the whirlpool catch them and then suck them down. It bothered her more than a little bit that this could also be used as a metaphor for her life.
-      </p>
-   
-
-
-    </div>
-    
-  </div>
-
-  
-</div>
-
-<Footer/>
-
-
-</div>
-
-
-  )
+  );
 }
