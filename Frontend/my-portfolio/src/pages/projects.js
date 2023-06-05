7ed193339c10d23a4291a221781e379fba044c87
Hiransanjeewa <hiransanjeewaa@gmail.com>
2023-06-05 13:53:11 +0530

    project_viewer components aded to project page

--- Frontend/my-portfolio/src/pages/projects.js
+++ Frontend/my-portfolio/src/pages/projects.js
@@ -3,8 +3,43 @@ import ProjectViewer from '../components/project_viewer'
 
 export default function Projects() {
   return (
-    <div>
-        <ProjectViewer/>
+
+
+
+
+   
+
+
+
+    <div class="container">
+  <div class="row justify-content-md-center">
+ <div >
+ <div class="col col-sm-4">
+    <ProjectViewer/>
     </div>
+    <div class="col-md-auto">
+    <ProjectViewer/>
+    </div>
+ </div>
+   
+  </div>
+  <div class="row">
+    <div class="col">
+    <ProjectViewer/>
+    </div>
+    <div class="col-md-auto">
+    <ProjectViewer/>
+    </div>
+    <div class="col col-lg-2">
+    <ProjectViewer/>
+    </div>
+  </div>
+</div>
+   
+
+
+
+
   )
 }
+
