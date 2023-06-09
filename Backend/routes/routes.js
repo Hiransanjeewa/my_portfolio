78cdc0e25099ed8b9d68c65cd371091d60796ca0
Basnayaka B.M.H.S <hiransanjeewaa@gmail.com>
2023-06-09 16:34:28 +0530

    Reinstalling Express js

--- Backend/routes/routes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express")
-    .Router()
-const controller = require('../index.js')
-
-router
-    .post('/', controller.createTask)
-
-    .get('/', controller.getTasks)
-module.export = router
