bb357d05a3e13f36c90ac6d773ea11de234eb258
Hiransanjeewa <hiransanjeewaa@gmail.com>
2023-06-09 17:29:11 +0530

    App js file creates , sample routes added

---
+++ Backend/app.js
@@ -0,0 +1,20 @@
+
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+// app.get('/hello', (req, res)=>{
+//     res.set('Content-Type', 'text/html');
+//     res.status(200).send("<h1>Hello GFG Learner!</h1>");
+// });
+
+const routes = require('./routes/routes')
+app.use('/', routes)
+app.listen(3000, (error) =>{
+        if(!error)
+            console.log("Server is Successfully Running, and App is listening on port "+ 3000)
+    else
+        console.log("Error occurred, server can't start", error);
+    }
+);
