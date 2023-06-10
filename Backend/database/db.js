82dbf2b8011ab36e38f5efcb73304f91b262ed72
Hiransanjeewa <hiransanjeewaa@gmail.com>
2023-06-10 17:02:52 +0530

    Backend coonected to Mongodb Database

---
+++ Backend/database/db.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const url = "mongodb://127.0.0.1:27017";
+
+mongoose.connect(url).then(() => {
+    console.log("Connected to Database");
+}).catch((err) => {
+    console.log("Not Connected to Database ERROR! ", err);
+});
