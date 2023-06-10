9a7910a52576edc2aa5aff795c6dc62d2f40605a
Hiransanjeewa <hiransanjeewaa@gmail.com>
2023-06-10 17:47:24 +0530

    App.js edited

--- Backend/app.js
+++ Backend/app.js
@@ -1,34 +1,17 @@
 
 const express = require('express');
 const db = require("./database/db.js")
-
-
-
 const app = express();
-
-
-
-// Connecting to mongo Db database
-
-
-
-
 const PORT = 3000;
 
 
-
-// app.get('/hello', (req, res)=>{
-//     res.set('Content-Type', 'text/html');
-//     res.status(200).send("<h1>Hello GFG Learner!</h1>");
-// });
-
-//app.use(express.json)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 const routes = require('./routes/routes')
 app.use('/', routes)
-app.listen(3000, (error) =>{
+app.listen(PORT, (error) =>{
         if(!error)
             console.log("Server is Successfully Running, and App is listening on port "+ 3000)
     else
