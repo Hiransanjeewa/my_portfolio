562632f399e19510002bf7bd69739320490dfde9
Hiransanjeewa <hiransanjeewaa@gmail.com>
2023-06-24 14:41:23 +0530

    connected to Medium Api , recied article data successfully

--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -6,6 +6,9 @@ const Db=require('../database/db')
 // Email services
 const EmailService = require('../services/emailService')
 const send = require("send");
+const axios = require("axios");
+
+const my_medium_id = '5bb14012dbd3';
 
 
 
@@ -33,14 +36,85 @@ router.use((req, res, next) => {
 // define the about route
 router.get('/checkAllEmails',async (req, res) => {
     // Requesting data from database
-    try {
-        const email=await Email.find()
-        res.json(email)
-    }catch (e) {
-        res.send('Error ' + e);
-    }
+    // try {
+    //     const email=await Email.find()
+    //     res.json(email)
+    // }catch (e) {
+    //     res.send('Error ' + e);
+    // }
    // res.send('About birds')
    // services("adssa","sfsdf","anbkj")
+
+
+
+
+
+
+
+
+
+
+    // const axios = require('axios');
+    //
+    // const options = {
+    //     method: 'GET',
+    //     url: 'https://medium2.p.rapidapi.com/article/5756b59e1138',
+    //     headers: {
+    //         'X-RapidAPI-Key': '78f26e88dcmshf87d454d9d409f3p101091jsned77dfdafda3',
+    //         'X-RapidAPI-Host': 'medium2.p.rapidapi.com'
+    //     }
+    // };
+    //
+    // try {
+    //     const response = await axios.request(options);
+    //     console.log(response.data);
+    // } catch (error) {
+    //
+    //     console.error(error);
+    // }
+
+// Content
+    // const axios = require('axios');
+    //
+    // const options = {
+    //     method: 'GET',
+    //     url: 'https://medium2.p.rapidapi.com/article/5756b59e1138/content',
+    //     headers: {
+    //         'X-RapidAPI-Key': '78f26e88dcmshf87d454d9d409f3p101091jsned77dfdafda3',
+    //         'X-RapidAPI-Host': 'medium2.p.rapidapi.com'
+    //     }
+    // };
+    //
+    // try {
+    //     const response = await axios.request(options);
+    //     console.log(response.data);
+    // } catch (error) {
+    //     console.error(error);
+    // }
+
+
+    // getting all publications
+    const axios = require('axios');
+
+    const options = {
+        method: 'GET',
+        url: 'https://medium2.p.rapidapi.com/user/5bb14012dbd3/articles',
+        headers: {
+            'X-RapidAPI-Key': '78f26e88dcmshf87d454d9d409f3p101091jsned77dfdafda3',
+            'X-RapidAPI-Host': 'medium2.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await axios.request(options);
+        console.log(response.data);
+    } catch (error) {
+        console.error(error);
+    }
+
+
+
+
 })
 
 router.post('/sendEmail',async (req, res) => {
