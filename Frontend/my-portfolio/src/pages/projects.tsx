4d7c41bbafc2da7d74e9abfb305b814c628adcea
Hiransanjeewa <hiransanjeewaa@gmail.com>
2023-07-15 22:17:34 +0530

    endpoint access point names changed

--- Frontend/my-portfolio/src/pages/projects.tsx
+++ Frontend/my-portfolio/src/pages/projects.tsx
@@ -37,7 +37,7 @@ const Projects: React.FC<Props> = () => {
   useEffect(() => {
     axios
     
-      .get('http://localhost:8080/getProjects?project='+project)
+      .get('http://0.0.0.0:8080/getProjects?project='+project)
       .then(response => {
         //console.log(response.data.length);
         let projectSet:React.ReactComponentElement<typeof Projectviewer, any>[] = [];
