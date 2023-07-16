6982fd84de4ec55b6cd6c4e916be623e113f1d01
Hiransanjeewa <hiransanjeewaa@gmail.com>
2023-07-16 09:44:15 +0530

    updated

--- Backend/database/db.js
+++ Backend/database/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const url = "mongodb://0.0.0.0:27017/my_portfolio";
 
+
 mongoose.connect(url).then(() => {
     console.log("Connected to Database");
 }).catch((err) => {
