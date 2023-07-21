8a68c0affec526084398c3232e51f55cd60afebe
Hiransanjeewa <hiransanjeewaa@gmail.com>
2023-07-21 23:19:37 +0530

    testing again

--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -47,7 +47,7 @@ router.get('/api/getProjects',async (req, res) => {
 
 
 // Automated Medium articles updater
-const j = schedule.scheduleJob({hour: 23, minute: 13}, () => {
+const j = schedule.scheduleJob({hour: 23, minute: 25}, () => {
     console.log('Job runs every day at 20.40AM');
 
     ArticleGetter.getArticles();
