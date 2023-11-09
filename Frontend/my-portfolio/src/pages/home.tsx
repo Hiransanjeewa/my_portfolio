10525b4fe5365524d7b36e3426adebc76f298b1f
Hiransanjeewa <hiransanjeewaa@gmail.com>
2023-11-09 12:44:47 +0530

    Home page profile pic updated

--- Frontend/my-portfolio/src/pages/home.tsx
+++ Frontend/my-portfolio/src/pages/home.tsx
@@ -40,6 +40,7 @@ const Home: React.FC<Props> = ({ htmlContent }) => {
         <div className="row">
           <div className="col col-lg-6 md-6" id='contents' style={style.contents}>
             {horizontalSpace}
+            
             <span style={{ fontSize: '150%' }}>Hi , I'm Hiran Sanjeewa,</span>
             <p id='caption-1'>
               Cloud & DevOps Enthusiast
@@ -58,7 +59,7 @@ const Home: React.FC<Props> = ({ htmlContent }) => {
             <div style={{ width: "12%" }}>
             </div>
             <div style={{ width: "76%", marginTop: "3%", backgroundColor: 'black' }}>
-              <img src={require('./my_profile_image.jpg')} alt="profile" style={{ width: '100%', height: '100%' }}/>
+              <img src={require('./profile_image_new.jpeg')} alt="profile" style={{ width: '100%', height: '90%' }}/>
               <br/>
             </div>
           </div>
